25630a6dc522fa744d9b489fa74eab2266804c2c
Zhekastorm <81801950+Zhekastorm@users.noreply.github.com>
2024-02-13 12:27:50 +0300

    Версия 2

---
+++ script.js
@@ -0,0 +1,13 @@
+// Slider
+
+$('.reviews__inner').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+});
+
+$('.reviews__inner--mobile').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
