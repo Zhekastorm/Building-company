5adbc459f575a8415ce71cfbe9939f2f8ecab338
Zhekastorm <81801950+Zhekastorm@users.noreply.github.com>
2024-02-15 14:02:56 +0300

    Версия 2

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ $('.reviews__inner').slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 3,
+  speed: 2000,
 });
 
 $('.reviews__inner--mobile').slick({
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
+  speed: 2000,
 });
