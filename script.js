c4887bcdf748a58db0ec9f0de4968b459d0f49a8
Zhekastorm <81801950+Zhekastorm@users.noreply.github.com>
2024-05-31 15:47:26 +0300

    Версия 5 Добавление модального окна

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const bodyHTML = document.querySelector('body');
 const header = document.querySelector('header');
-
+let scrollPrev = 0
 const burgerBtn = document.getElementById('check__menu');
 const mainMenu = document.getElementById('menuNav');
 const main = document.querySelector('.main');
+const modalBtnOpen = document.querySelectorAll("[data-modal-button]")
+const modalBtnClose = document.querySelectorAll('.modal__close-btn')
+const modalWindowAll = document.querySelectorAll('[data-modal]')
+const formBtn = document.querySelectorAll('#formBtn')
 
 
 // Header Fixed
+$(window).scroll(function (){
+  const scrolled = $(window).scrollTop()
 
+  if (scrolled > 80 && scrolled > scrollPrev){
+    header.classList.add('hidden')
+  } else {
+    header.classList.remove('hidden')
+  }
+  scrollPrev = scrolled;
+})
 
 
 // Nav Menu
@@ -38,3 +51,37 @@ $('.reviews__inner--mobile').slick({
   slidesToScroll: 1,
   speed: 2000,
 });
+
+//Popup
+modalBtnOpen.forEach(function (item){
+  item.addEventListener('click', function(){
+    const modalID = this.dataset.modalButton;
+    console.log(modalID)
+
+    const modalCorrect = document.querySelector('#' + modalID)
+    modalCorrect.classList.remove('hidden')
+    bodyHTML.classList.add('hidden')
+
+    modalCorrect.querySelector('.modal__window').addEventListener('click', function (event){
+      event.stopPropagation()
+    })
+  })
+})
+
+modalBtnClose.forEach(function (item){
+  item.addEventListener('click', function (){
+    const modalCorrect = this.closest('[data-modal]')
+    modalCorrect.classList.add('hidden')
+    bodyHTML.classList.remove('hidden')
+  })
+})
+modalWindowAll.forEach(function(item){
+  item.addEventListener('click', function (){
+    this.classList.add('hidden')
+    bodyHTML.classList.remove('hidden')
+  })
+})
+
+
+
+
